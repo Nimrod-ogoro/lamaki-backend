f786f2f34b79e7b3edc9fa0455659029cc27314e
Nimrod-ogoro <nimrodogoro@gmail.com>
2025-09-19 11:38:01 +0300

    wow

--- src/db.js
+++ src/db.js
@@ -1,9 +1,13 @@
-// src/db.
+// src/db.js
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // necessary for Vercel/Postgres connections
+  },
 });
 
 module.exports = pool;
+
