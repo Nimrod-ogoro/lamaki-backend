e9e425bc5f9a018ee05010b381e75710fb39d8d9
Nimrod-ogoro <nimrodogoro@gmail.com>
2025-09-19 14:33:20 +0300

    commit

--- src/r2.js
+++ src/r2.js
@@ -1,27 +1,20 @@
-const express = require("express");
-const router = express.Router();
-const projectsController = require("../controllers/projectsController");
-const multer = require("multer");
-const multerS3 = require("multer-s3");
 const AWS = require("aws-sdk");
 
-// Cloudflare R2 config
 const s3 = new AWS.S3({
-  endpoint: new AWS.Endpoint(process.env.R2_ENDPOINT), // e.g. https://<account_id>.r2.cloudflarestorage.com
+  endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   accessKeyId: process.env.R2_ACCESS_KEY,
   secretAccessKey: process.env.R2_SECRET_KEY,
-  region: "auto",
 });
 
-// Multer S3 setup
-const upload = multer({
-  storage: multerS3({
-    s3,
-    bucket: process.env.R2_BUCKET, // e.g. "lamaki"
-    acl: "public-read",
-    key: function (req, file, cb) {
-      cb(null, `${Date.now()}_${file.originalname}`);
-    },
-  }),
-});
+if (req.file) {
+  const params = {
+    Bucket: process.env.R2_BUCKET,
+    Key: Date.now() + path.extname(req.file.originalname),
+    Body: req.file.buffer,
+    ContentType: req.file.mimetype,
+    ACL: "public-read",
+  };
+  const data = await s3.upload(params).promise();
+  image_url = data.Location; // this goes to DB
+}
 
